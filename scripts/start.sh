e29cd9296af3ddd583a1cfa72054b5a559d8df03
brad <34373485+baxtrax@users.noreply.github.com>
2023-09-30 10:03:08 -0400

    fixed scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+SCRIPT_DIR="$(pwd)"
 echo "Starting container..."
 echo "Clearing log file..."
 > log.txt
 echo "Running image..."
-docker run -d -p $(cat ../.env | grep APP_PORT= | cut -d: -f2 | awk '/^/ { print $1":"$1 }') -v ../log.txt:/usr/src/app/log.txt --name login-api ccu-login-api
+docker run -d -p $(cat $SCRIPT_DIR/../.env | grep APP_PORT= | cut -d: -f2 | awk '/^/ { print $1":"$1 }') -v $SCRIPT_DIR/../log.txt:/usr/src/app/log.txt --name login-api ccu-login-api
 echo "Container started"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Killing and removing container...
+echo "Killing and removing container..."
 docker kill login-api
 docker rm login-api
 echo "Container stopped"
