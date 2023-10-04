79a248eb8ec8bc54c292987a665a3cf7ac962430
brad <34373485+baxtrax@users.noreply.github.com>
2023-10-04 19:42:41 -0400

    fixed scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,5 +5,11 @@ echo "Starting container..."
 echo "Clearing log file..."
 > $SCRIPT_DIR/../log.txt
 echo "Running image..."
-docker run -d -p $(cat $SCRIPT_DIR/../.env | grep APP_PORT= | cut -d: -f2 | awk '/^/ { print $1":"$1 }') -v $SCRIPT_DIR/../log.txt:/usr/src/app/log.txt --name login-api ccu-login-api
+docker run -d $(
+if [ -f "$SCRIPT_DIR/../.env" ]; then
+    grep -oP 'APP_PORT=\K\d+' $SCRIPT_DIR/../.env | awk '{ print "-p "$1":"$1 }'
+else
+    echo "-p 8084:8084"
+fi
+) -v $SCRIPT_DIR/../log.txt:/usr/src/app/log.txt --name login-api ccu-login-api
 echo "Container started"
